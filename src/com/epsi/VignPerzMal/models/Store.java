2ae928e547f5a45d5bf137766e28b7676fb54c51
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2014-02-18 18:01:26 +0100

    Création de l'entité Store

--- src/com/epsi/VignPerzMal/models/Store.java
+++ src/com/epsi/VignPerzMal/models/Store.java
@@ -1,145 +1,69 @@
 package com.epsi.VignPerzMal.models;
 
-public class StoreModel {
+public class Store {
 	
-	 private String codeMag;
-	 private String libelle;
-	 private String adresse;
-	 private double codePostal;
-	 private String ville;
-	 private String telephone;
-	 private String horaires;
-	 private String fax;
-	 private String latitude;
-	 private String longitude;
-	 
-	 
-	/**
-	 * @return the longitude
-	 */
-	public String getLongitude() {
-		return longitude;
+	private String codeMag;
+	private String name;
+	private String address;
+	private String zipCode;
+	private String city;
+	private String phone;
+	private String schedule;
+	private String fax;
+	private String latitude;
+	private String longitude;
+
+	public Store() {
 	}
-	/**
-	 * @param longitude the longitude to set
-	 */
-	public void setLongitude(String longitude) {
+	
+	public Store(String codeMag, String name, String address, String zipCode,
+			String city, String phone, String schedule, String fax,
+			String latitude, String longitude) {
+		super();
+		this.codeMag = codeMag;
+		this.name = name;
+		this.address = address;
+		this.zipCode = zipCode;
+		this.city = city;
+		this.phone = phone;
+		this.schedule = schedule;
+		this.fax = fax;
+		this.latitude = latitude;
 		this.longitude = longitude;
 	}
-	/**
-	 * @return the codeMag
-	 */
-	public String getCodeMag() {
-		return codeMag;
+
+	public String getCodeMag() { return codeMag; }
+	public void setCodeMag(String codeMag) { this.codeMag = codeMag; }
+
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+
+	public String getAddress() { return address; }
+	public void setAddress(String address) { this.address = address; }
+
+	public String getZipCode() { return zipCode; }
+	public void setZipCode(String zipCode) { this.zipCode = zipCode; }
+
+	public String getCity() { return city; }
+	public void setCity(String city) { this.city = city; }
+
+	public String getPhone() { return phone; }
+	public void setPhone(String telephone) { this.phone = telephone; }
+
+	public String getSchedule() { return schedule; }
+	public void setSchedule(String schedule) { this.schedule = schedule; }
+
+	public String getFax() { return fax; }
+	public void setFax(String fax) { this.fax = fax; }
+
+	public String getLatitude() { return latitude; }
+	public void setLatitude(String latitude) { this.latitude = latitude; }
+
+	public String getLongitude() { return longitude; }
+	public void setLongitude(String longitude) { this.longitude = longitude; }
+
+	@Override
+	public String toString() {
+		return this.name;
 	}
-	/**
-	 * @param codeMag the codeMag to set
-	 */
-	public void setCodeMag(String codeMag) {
-		this.codeMag = codeMag;
-	}
-	/**
-	 * @return the libelle
-	 */
-	public String getLibelle() {
-		return libelle;
-	}
-	/**
-	 * @param libelle the libelle to set
-	 */
-	public void setLibelle(String libelle) {
-		this.libelle = libelle;
-	}
-	/**
-	 * @return the adresse
-	 */
-	public String getAdresse() {
-		return adresse;
-	}
-	/**
-	 * @param adresse the adresse to set
-	 */
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
-	/**
-	 * @return the codePostal
-	 */
-	public double getCodePostal() {
-		return codePostal;
-	}
-	/**
-	 * @param codePostal the codePostal to set
-	 */
-	public void setCodePostal(double codePostal) {
-		this.codePostal = codePostal;
-	}
-	/**
-	 * @return the ville
-	 */
-	public String getVille() {
-		return ville;
-	}
-	/**
-	 * @param ville the ville to set
-	 */
-	public void setVille(String ville) {
-		this.ville = ville;
-	}
-	/**
-	 * @return the telephone
-	 */
-	public String getTelephone() {
-		return telephone;
-	}
-	/**
-	 * @param telephone the telephone to set
-	 */
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-	/**
-	 * @return the horaires
-	 */
-	public String getHoraires() {
-		return horaires;
-	}
-	/**
-	 * @param horaires the horaires to set
-	 */
-	public void setHoraires(String horaires) {
-		this.horaires = horaires;
-	}
-	/**
-	 * @return the fax
-	 */
-	public String getFax() {
-		return fax;
-	}
-	/**
-	 * @param fax the fax to set
-	 */
-	public void setFax(String fax) {
-		this.fax = fax;
-	}
-	/**
-	 * @return the latitude
-	 */
-	public String getLatitude() {
-		return latitude;
-	}
-	/**
-	 * @param latitude the latitude to set
-	 */
-	public void setLatitude(String latitude) {
-		this.latitude = latitude;
-	}
-	
-	// Will be used by the ArrayAdapter in the ListView
-	  @Override
-	  public String toString() {
-	    return adresse;
-	  }
-	 
-	 
 }
