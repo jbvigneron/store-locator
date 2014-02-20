f345d801270a44c81ff66d4df9147fa0c91b669d
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2014-02-20 18:58:43 +0100

    Modification du contrôleur pour la gestion de la base de données

--- src/com/epsi/VignPerzMal/controllers/StoresProviderController.java
+++ src/com/epsi/VignPerzMal/controllers/StoresProviderController.java
@@ -3,13 +3,43 @@ package com.epsi.VignPerzMal.controllers;
 import java.net.URL;
 import java.util.AbstractList;
 
+import android.content.Context;
+
+import com.epsi.VignPerzMal.database.DAL;
+import com.epsi.VignPerzMal.database.StoreDAL;
+import com.epsi.VignPerzMal.helpers.NetworkInfoHelper;
 import com.epsi.VignPerzMal.models.Store;
 import com.epsi.VignPerzMal.parser.FacadeParser;
 
-public class StoreProviderController {
+public class StoresProviderController {
 
-	public AbstractList<Store> retrieve(URL url){
-		FacadeParser facade = new FacadeParser();
-		return facade.parse(url);
+	public AbstractList<Store> retrieve(Context context, URL url) {
+		AbstractList<Store> stores;
+		DAL<Store> dal;
+
+		/* Check if network is available.
+		 * If yes, retrieve stores from remote JSON feed
+		 * If not, retrieve stores from database
+		 */
+		if(NetworkInfoHelper.isNetworkAvailable(context)) {
+			FacadeParser facade = new FacadeParser();
+			stores = facade.parse(url);
+
+			if(stores != null) {
+				dal = new StoreDAL(context);
+				dal.open();
+				dal.clear();
+				
+				for(Store store : stores) {
+					dal.insert(store);
+				}
+			}
+		}
+		else {	
+			dal = new StoreDAL(context);
+			return dal.get();
+		}
+
+		return stores;
 	}
 }
