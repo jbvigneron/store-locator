5d9f268d691efd2196639c05283f70a579cc0e2e
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2014-03-12 23:18:28 +0100

    Debut code behind de la page d'accueil

--- src/com/epsi/VignPerzMal/activities/MainActivity.java
+++ src/com/epsi/VignPerzMal/activities/MainActivity.java
@@ -4,12 +4,39 @@ import com.epsi.VignPerzMal.storelocator.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private TextView tvZipCode;
+	private Button btnSearch;
+	private Button btnAroundMe;
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_main);
+		
+		// Get controls
+		tvZipCode = (TextView)findViewById(R.id.tvZipCode);
+		btnSearch = (Button)findViewById(R.id.btnSearch);
+		btnAroundMe = (Button)findViewById(R.id.btnAroundMe);
+		
+		// Create events
+		btnSearch.setOnClickListener(this);
+		btnAroundMe.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if(v == btnSearch) {
+			// TODO: Launch search by ZIP Code
+		}
+		else if(v == btnAroundMe) {
+			// TODO: Launch search around the user
+		}
 	}
 }
