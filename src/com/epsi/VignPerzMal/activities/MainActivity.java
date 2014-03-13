a60b78d859cab9d4cec05ba28ace13c21a63ba79
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2014-03-13 17:11:01 +0100

    Recherche par code postal terminee

--- src/com/epsi/VignPerzMal/activities/MainActivity.java
+++ src/com/epsi/VignPerzMal/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.epsi.VignPerzMal.activities;
 
 import java.util.AbstractList;
+import java.util.ArrayList;
 
 import com.epsi.VignPerzMal.adapters.StoreAdapterProvider;
 import com.epsi.VignPerzMal.database.DAL;
@@ -20,19 +21,25 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	private TextView tvZipCode;
+	private TextView etZipCode;
 	private ImageButton btnSearch;
 	private ImageButton btnAroundMe;
 	private Button btnMap;
 	private ListView lvStores;
+	
+	private DAL<Store> dal;
+	private AbstractList<Store> stores;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_main);
+		
+		dal = new StoreDAL(getApplicationContext());
+		stores = new ArrayList<Store>();
 
 		// Get controls
-		tvZipCode = (TextView)findViewById(R.id.tvZipCode);
+		etZipCode = (TextView)findViewById(R.id.etZipCode);
 		btnSearch = (ImageButton)findViewById(R.id.btnSearch);
 		btnAroundMe = (ImageButton)findViewById(R.id.btnAroundMe);
 		btnMap = (Button)findViewById(R.id.btnMap);
@@ -47,26 +54,43 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	protected void onStart() {
 		super.onStart();
+
+		stores = dal.get();
 		
-		// Load stores from database
-		DAL<Store> dal = new StoreDAL(getApplicationContext());
-		AbstractList<Store> stores = dal.get();
-		
-		StoreAdapterProvider adapterProvider = new StoreAdapterProvider();
-		ListAdapter adapter = adapterProvider.adapt(getApplicationContext(), stores);
-		lvStores.setAdapter(adapter);
+		displayStores();
 	}
 
 	@Override
 	public void onClick(View v) {
-		if(v == btnSearch) {
-			// TODO: Launch search by ZIP Code
-		}
-		else if(v == btnAroundMe) {
-			// TODO: Launch search around the user
-		}
-		else if(v == btnMap) {
-			// TODO: Show stores on Google Maps
-		}
+		if(v == btnSearch)
+			searchbyZipCode();
+		else if(v == btnAroundMe)
+			searchAroundMe();
+		else if(v == btnMap)
+			showMap();
+	}
+	
+	private void displayStores() {
+		StoreAdapterProvider adapterProvider = new StoreAdapterProvider();
+		ListAdapter adapter = adapterProvider.adapt(getApplicationContext(), stores);
+		lvStores.setAdapter(adapter);
+	}
+	
+	private void searchbyZipCode() {
+		
+		String zipCode = etZipCode.getText().toString();
+		stores = dal.search(zipCode);
+		
+		displayStores();
+	}
+	
+	private void searchAroundMe() {
+		
+		// TODO: Launch search around the user
+	}
+	
+	private void showMap() {
+		
+		// TODO: Show stores on Google Maps
 	}
 }
